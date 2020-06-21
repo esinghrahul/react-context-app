551d9cc48d6d70015970e2e16e2da34ad9517db2
esinghrahul <e.singh.rahul@outlook.com>
2020-06-21 11:50:14 +0530

    converting booklist component to use useContext hook alongside context API

--- src/components/booklist.component.jsx
+++ src/components/booklist.component.jsx
@@ -1,7 +1,9 @@
-import React, { Component } from 'react'
+import React, { Component, useContext } from 'react'
 
 import {ThemeContext} from '../contexts/ThemeContext'
 
+/*
+//Implementing context using static contextType approach that is only viable for class based components
 export default class BookList extends Component{
     //Can only use this approach of static context types with class based components and if there aren't multiple contexts
     static contextType = ThemeContext
@@ -19,3 +21,20 @@ export default class BookList extends Component{
         )
     }
 }
+*/
+
+//Implementing context using useContext hook
+const BookList = () => {
+    const {isLightTheme, light, dark} =  useContext(ThemeContext)
+    const theme = isLightTheme ? light : dark
+    return(
+        <div className='book-list' style = {{color: theme.textStyle, background: theme.bg}}>
+            <ul>
+                <li style={{background: theme.ui}}>The Way of the Kings</li>
+                <li style={{background: theme.ui}}>The Name of the Wind</li>
+                <li style={{background: theme.ui}}>The Final Empire</li>
+            </ul>
+        </div>
+    )
+}
+export default BookList
